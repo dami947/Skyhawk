4a5d24a232a76271fca0a2796f2ff04afe58dc27
Damian <tobres.dk@gmail.com>
2022-03-16 22:26:55 +0100

    add page

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,26 @@
 const navMobile = document.querySelector('.nav-mobile');
 const navAllLink = document.querySelectorAll('.nav-link')
 const navBtn = document.querySelector('.btn-bars');
+const allSections = document.querySelectorAll('.section');
 const footerYear = document.querySelector('.footer-year');
-
-
-
 
 
 const handleNav = () => {
     document.body.classList.toggle('sticky-body')
     navMobile.classList.toggle('mobile-active')
-    navBtn.classList.toggle('black-color')
+    navBtn.classList.add('black-color')
+}
+
+const handleObserver = () => {
+    const currentSection = window.scrollY;
+
+    allSections.forEach(section => {
+        if (section.classList.contains('section-light') && section.offsetTop <= currentSection) {
+            navBtn.classList.add('black-color')
+        } else if (!section.classList.contains('section-light') && section.offsetTop <= currentSection) {
+            navBtn.classList.remove('black-color')
+        }
+    })
 }
 
 
@@ -18,6 +28,7 @@ const handleCurrentYear = () => {
     const year = (new Date).getFullYear();
     footerYear.innerText = year;
 }
-handleCurrentYear();
 
+handleCurrentYear();
 navBtn.addEventListener('click', handleNav);
+window.addEventListener('scroll', handleObserver);
